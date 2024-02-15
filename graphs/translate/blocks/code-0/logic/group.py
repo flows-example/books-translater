d0394cc76afe4ca006691d98aaa9402dd77319a9
taozeyu <i@taozeyu.com>
2024-02-16 06:48:01 +0800

    refactor(translate): replace p

--- graphs/translate/blocks/code-0/logic/group.py
+++ graphs/translate/blocks/code-0/logic/group.py
@@ -1,31 +1,39 @@
 from .paragraph_sliter import split_paragraph
 
+class Paragraph:
+  def __init__(self, text: str, index: int):
+    self.text: str = text
+    self.index: int = index
+
 class ParagraphsGroup:
   def __init__(self, max_paragraph_len: int, max_group_len: int):
     self.max_paragraph_len: int = max_paragraph_len
     self.max_group_len: int = max_group_len
 
-  def split(self, paragraph_list: list):
-    splited_paragraph_list = []
+  def split(self, text_list: list[str]) -> list[list[Paragraph]]:
+    splited_paragraph_list: list[Paragraph] = []
 
-    for paragraph in paragraph_list:
-      if len(paragraph) > self.max_paragraph_len:
-        for cell in split_paragraph(paragraph):
-          splited_paragraph_list.append(cell)
+    for index, text in enumerate(text_list):
+      if len(text) > self.max_paragraph_len:
+        for cell in split_paragraph(text):
+          splited_paragraph_list.append(Paragraph(cell, index))
       else:
-        while len(paragraph) > self.max_group_len:
-          splited_paragraph_list.append(paragraph[:self.max_group_len])
-          paragraph = paragraph[self.max_group_len:]
-        if len(paragraph) > 0:
-          splited_paragraph_list.append(paragraph)
+        while len(text) > self.max_group_len:
+          splited_paragraph_list.append(Paragraph(
+            text=text[:self.max_group_len], 
+            index=index,
+          ))
+          text = text[self.max_group_len:]
+        if len(text) > 0:
+          splited_paragraph_list.append(Paragraph(text, index))
 
     sum_len = 0
     self_paragraphs_count = 0
-    grouped_paragraph_list = []
-    current_paragraph_list = []
+    grouped_paragraph_list: list[list[Paragraph]] = []
+    current_paragraph_list: list[Paragraph] = []
 
     for paragraph in splited_paragraph_list:
-      paragraph_len = len(paragraph)
+      paragraph_len = len(paragraph.text)
 
       if len(current_paragraph_list) > 0 and sum_len + paragraph_len > self.max_group_len:
         grouped_paragraph_list.append(current_paragraph_list)
@@ -38,7 +46,7 @@ class ParagraphsGroup:
         else:
           current_paragraph_list = current_paragraph_list[-2:]
           for cell in current_paragraph_list:
-            sum_len += len(cell)
+            sum_len += len(cell.text)
 
       sum_len += paragraph_len
       self_paragraphs_count += 1
--- graphs/translate/blocks/code-0/logic/translator.py
+++ graphs/translate/blocks/code-0/logic/translator.py
@@ -27,6 +27,10 @@ class _XML:
 
     text = etree.tostring(self.root, method="html", encoding="utf-8")
     text = text.decode("utf-8")
+
+    # TODO: 替换成更完善的自闭检测
+    # link 可能生成非自闭 tag，对 epub 是非法，此处用正则替换掉非自闭型
+    text = re.sub(r"<((link|meta)[^>]*?)(?<!/)>", r"<\1/>", text)
     text = self.head + text
 
     return text
@@ -57,50 +61,116 @@ class Translator:
       return text
 
   def translate_page(self, page_content):
+    xml = _XML(page_content, self.parser)
+    source_text_list: list[str] = []
+
+    for p_dom in xml.root.xpath('//p'):
+      bin_text = etree.tostring(p_dom, method="html", encoding="utf-8")
+      source_text_list.append(bin_text.decode("utf-8"))
+
     group = ParagraphsGroup(
       max_paragraph_len=self.max_paragraph_characters,
       # https://support.google.com/translate/thread/18674882/how-many-words-is-maximum-in-google?hl=en
       max_group_len=5000,
     )
-    # to remove <?xml version="1.0" encoding="utf-8"?> which lxml cannot parse
-    # xml = re.sub(r"^<\?xml.*\?>", "", page_content)
-    # remove namespace of epub
-    # xml = re.sub(r"xmlns=\"http://www.w3.org/1999/xhtml\"", "", xml)
-    # xml = re.sub(r"xmlns:epub=\"http://www.idpf.org/2007/ops\"", "", xml)
-    # xml = re.sub(r"epub:", "", xml)
+    to_target_text_pair_map: dict[int, list[list[str]]] = {}
 
-    xml = _XML(page_content, self.parser)
-    body_dom = xml.root.find("body")
+    for paragraph_list in group.split(source_text_list):
+      source_text_list = list(map(lambda x: self._clean_p_tag(x.text), paragraph_list))
+      target_text_list = self._translate_text_list(source_text_list)
 
-    merged_text_list = []
-    source_text_list, child_doms = self._collect_child_text_list(body_dom)
-    source_text_groups = group.split(source_text_list)
+      for i, target_text in enumerate(target_text_list):
+        index = paragraph_list[i].index
+        source_text = source_text_list[i]
+        if self.clean_format:
+          target_text = escape(target_text)
+        else:
+          target_text = self._clean_p_tag(target_text)
 
-    for child_dom in child_doms:
-      body_dom.remove(child_dom)
+        pair = [source_text, target_text]
 
-    for index, source_text_list in enumerate(source_text_groups):   
-      source_text_list = self._standardize_paragraph_list(source_text_list)
-      target_text_list = self._translate_html(source_text_list)
+        if index in to_target_text_pair_map:
+          # TODO: 判断是否有重复的 text 作为 debug
+          to_target_text_pair_map[index].append(pair)
+        else:
+          to_target_text_pair_map[index] = [pair]
 
-      if index > 0:
-        source_text_list.pop(0)
-        target_text_list.pop(0)
+    for index, p_dom in enumerate(xml.root.xpath('//p')):
+      new_p_doms = []
+      for pair in to_target_text_pair_map[index]:
+        for text in pair:
+          new_p_dom = create_node(f"<p>{text}</p>", parser=self.parser)
+          new_p_doms.append(new_p_dom)
+  
+      parent_dom = p_dom.getparent()
+      index_at_parent = parent_dom.index(p_dom)
 
-      # 长度为 2 的数组来源于裁剪，不得已，此时它的后继的首位不会与它重复，故不必裁剪
-      if index < len(source_text_groups) and len(source_text_list) > 2:
-        source_text_list.pop()
-        target_text_list.pop()
-
-      for source, target in zip(source_text_list, target_text_list):
-        source_dom = create_node(source, parser=self.parser)
-        target_dom = create_node(target, parser=self.parser)
-
-        if source_dom is not None and target_dom is not None:
-          body_dom.append(source_dom)
-          body_dom.append(target_dom)
+      for new_p_dom in reversed(new_p_doms):
+        parent_dom.insert(index_at_parent, new_p_dom)
+      parent_dom.remove(p_dom)
 
     return xml.encode()
+
+  def _translate_text_list(self, source_text_list):
+    target_text_list = [""] * len(source_text_list)
+    to_translated_text_list = []
+    index_list = []
+
+    for index, text in enumerate(source_text_list):
+      if text != "" and not re.match(r"[\s\n]+", text):
+        text = f"<p>{text}</p>"
+        if self.clean_format:
+          dom = create_node(text, parser=self.parser)
+          text = etree.tostring(dom, method="text", encoding="utf-8", pretty_print=False)
+          text = text.decode("utf-8")
+        to_translated_text_list.append(text)
+        index_list.append(index)
+    
+    if self.clean_format:
+      mime_type = "text/plain"
+    else:
+      mime_type = "text/html"
+
+    for i, text in enumerate(self._translate_by_google(to_translated_text_list, mime_type)):
+      index = index_list[i]
+      target_text_list[index] = text
+
+    return target_text_list
+
+    # body_dom = xml.root.find("body")
+    # merged_text_list = []
+    # source_text_list, child_doms = self._collect_child_text_list(body_dom)
+    # source_text_groups = group.split(source_text_list)
+
+    # for child_dom in child_doms:
+    #   body_dom.remove(child_dom)
+
+    # for index, source_text_list in enumerate(source_text_groups):   
+    #   source_text_list = self._standardize_paragraph_list(source_text_list)
+    #   target_text_list = self._translate_html(source_text_list)
+
+    #   if index > 0:
+    #     source_text_list.pop(0)
+    #     target_text_list.pop(0)
+
+    #   # 长度为 2 的数组来源于裁剪，不得已，此时它的后继的首位不会与它重复，故不必裁剪
+    #   if index < len(source_text_groups) and len(source_text_list) > 2:
+    #     source_text_list.pop()
+    #     target_text_list.pop()
+
+    #   for source, target in zip(source_text_list, target_text_list):
+    #     source_dom = create_node(source, parser=self.parser)
+    #     target_dom = create_node(target, parser=self.parser)
+
+    #     if source_dom is not None and target_dom is not None:
+    #       body_dom.append(source_dom)
+    #       body_dom.append(target_dom)
+
+  def _clean_p_tag(self, text: str) -> str:
+    text = re.sub(r"^[\s\n]*<p[^>]*>", "", text)
+    text = re.sub(r"</\s*p>[\s\n]*$", "", text)
+    text = re.sub(r"[\s\n]+", " ", text)
+    return text
 
   def _collect_child_text_list(self, dom):
     text_list = []
